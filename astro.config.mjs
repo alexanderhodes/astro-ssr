dcac558553be436f217f5f588f019447e7a2ff17
alexanderhodes <alexander.hodes@live.com>
2023-12-23 14:17:10 +0100

    chore: added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,23 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://astro-ssr-psi.vercel.app",
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "de",
+        locales: {
+          de: "de-DE",
+          en: "en-US",
+        },
+      },
+    }),
+  ],
   i18n: {
     defaultLocale: "de",
     locales: ["de", "en"],
