bd41701cf13e306e6806f21aa034ea4d2a075009
alexanderhodes <alexander.hodes@live.com>
2023-12-23 13:46:00 +0100

    chore: added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +13,6 @@ export default defineConfig({
     },
   },
   prefetch: true,
+  output: "hybrid",
+  adapter: vercel(),
 });
