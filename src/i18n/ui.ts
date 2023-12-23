e61d91343df848b3ea7d8d646338ade556d7a214
alexanderhodes <alexander.hodes@live.com>
2023-12-23 11:16:01 +0100

    feat: added i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  de: "Deutsch",
+  en: "English",
+};
+
+export const defaultLang = "de";
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.twitter": "Twitter",
+  },
+  fr: {
+    "nav.home": "Accueil",
+    "nav.about": "À propos",
+  },
+} as const;
