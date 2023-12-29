70ccddcafc13d1b44d5d8e7c7740966c8dceab71
alexanderhodes <alexander.hodes@live.com>
2023-12-29 14:08:31 +0100

    feat: updated content

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -17,7 +17,10 @@ export default config({
           formatting: true,
           dividers: true,
           links: true,
-          images: true,
+          images: {
+            directory: "public/images/posts",
+            publicPath: "/images/posts",
+          },
         }),
       },
     }),
