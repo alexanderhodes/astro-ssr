4c6571e91f4bad44329725c9da7b24c22f5b9bb9
alexanderhodes <alexander.hodes@live.com>
2023-12-29 12:47:46 +0100

    feat: added keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,36 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-ssr-psi.vercel.app",
-  integrations: [tailwind(), sitemap({
-    i18n: {
-      defaultLocale: "de",
-      locales: {
-        de: "de-DE",
-        en: "en-US"
-      }
-    }
-  }), react()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "de",
+        locales: {
+          de: "de-DE",
+          en: "en-US",
+        },
+      },
+    }),
+    react(),
+    markdoc(),
+    keystatic(),
+  ],
   i18n: {
     defaultLocale: "de",
     locales: ["de", "en"],
     routing: {
-      prefixDefaultLocale: false
-    }
+      prefixDefaultLocale: false,
+    },
   },
   prefetch: true,
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel(),
 });
